0812edbdfb78e202cad6dbb1db5ac635b6628bb2
siansoft <siansoft@hotmail.com>
2019-10-13 21:29:41 +0300

    get to last update

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,7 +6,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
     }
         console.log('Connect to Mongodb Server');
         const db = client.db('TodoApp');
-        db.collection('Todos').insertOne({
+        /*db.collection('Todos').insertOne({
             text: 'Something to do',
             completed: 'false'
         }, (err, result)=>{
@@ -15,6 +15,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
             }
             console.log(JSON.stringify(result.ops, undefined, 2));
         });
+        */
+        db.collection('Users').insertOne({
+            name: 'naser',
+            age: 30,
+            location: 'Kuwait'
+        }, (err, result) => {
+            if (err) {
+                return console.log('Unable to insert todo', err);
+            }
+                console.log(result.ops);
+        }); 
         client.close();
         //console.log('connection to Mongodb Server Closed');
 
