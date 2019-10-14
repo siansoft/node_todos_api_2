87e28b7e8e795c2f54ef62dafd9cbfae2a1d7753
siansoft <siansoft@hotmail.com>
2019-10-14 20:00:21 +0300

    Update mongodb-connect.js

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -5,18 +5,36 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
         return console.log('Unable to connect to Mongodb Server');
     }
         console.log('Connect to Mongodb Server');
-        const db = client.db('TodoApp');
-        /*db.collection('Todos').insertOne({
-            text: 'Something to do',
-            completed: 'false'
-        }, (err, result)=>{
-            if(err){
-                return console.log('Unable to insert todo', err);
-            }
-            console.log(JSON.stringify(result.ops, undefined, 2));
-        });
-        */
-        db.collection('Users').insertOne({
+        const db = client.db('TodoApp'); // Connecting the the DB
+
+        //Connecting to collaction Table and insert data
+        //db.collection('Todos').insertOne({
+            //text: 'going to market',
+           // completed: 'false'
+        //}, (err, result)=>{
+            //if(err){
+               // return console.log('Unable to insert todo', err);
+            //}
+            //console.log(JSON.stringify(result.ops, undefined, 2));
+        //});
+
+        //Connecting to collaction Table and faching data finding
+        //db.collection('Todos').find().toArray().then((doc)=>{
+           // console.log('ToDos');
+           // console.log(doc);
+       // },(err)=>{
+          //  console.log('Unble to get data', err);
+        //})
+        
+
+        //makaing quiree 
+        db.collection('Todos').find({completed : false}).toArray().then((doc)=>{
+            console.log('ToDos');
+            console.log(doc);
+        },(err)=>{
+            console.log('Unble to get data', err);
+        })
+        /*db.collection('Users').insertOne({
             name: 'Mohd',
             age: 52,
             location: 'Kuwait'
@@ -32,7 +50,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
                 console.log(obj);
                 console.log(obj.getTimestamp());
         });
-             
+        */     
         client.close();
         //console.log('connection to Mongodb Server Closed');
 
